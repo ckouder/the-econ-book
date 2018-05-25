46d49bc5f337cbbb6337c128d3dd139989b83496
ckouder <guobingji@126.com>
2018-05-26 06:51:00 +0800

    a little step for me

--- theEconBook/classifier/static/main.js
+++ theEconBook/classifier/static/main.js
@@ -8,36 +8,87 @@ window.onload = function() {
 
     class RadarGraph {
         constructor (data) {
+            this.data = data;
             this.labels = Object.keys(data);
-            this.axisPolygons = this.drawAxis(data);
+            this.axes = this.drawAxis(data);
         }
         drawAxis(data) {
             var numOfSides = this.labels.length,
-                sideLength = 50,
-                number = 5;
-            
-            var axisPolygons = new paper.Group();
+                sideLength = paper.view.size.width / 20,
+                number = 4,
+                strokeColor = 'black';
+
+            // draw shape axes and labels
+            var axesGroup = new paper.Group(),
+                horizontalAxisGroup = new paper.Group(),
+                labelGroup = new paper.Group(), 
+                vertices = [];
+
             for (var i = 0; i <= number; i++) {
-                var polygon = new paper.Path.RegularPolygon(paper.view.center, numOfSides, sideLength * i);
-                polygon.strokeColor = "black";
+                var horizontalAxis = new paper.Path.RegularPolygon({
+                        center: paper.view.center, 
+                        sides: numOfSides, 
+                        radius: sideLength * i,
+                        strokeColor: strokeColor,
+                        name: numOfSides + '-' + i
+                    });
+                
+                if (horizontalAxis !== 0 && horizontalAxis.length >= numOfSides) {
+                    for (var j = 0; j <= numOfSides; j ++) {
+                        //console.log(j, horizontalAxis.length);
+                        var textPoistion = j * horizontalAxis.length / numOfSides;
+                        
+                        vertices.push(horizontalAxis.getPointAt(textPoistion));
 
-                axisPolygons.addChild(polygon);
+                        if (i === number) {
+                            var label = this.drawLabel(this.labels[j], textPoistion, horizontalAxis);
+                            labelGroup.addChild(label);
+                        }
+                    }
+                } else {
+                    vertices.push(horizontalAxis.getPointAt(0));
+                }
+
+                labelGroup.scale(1.1);
+                labelGroup.translate(new paper.Point(-20,3));
+                horizontalAxisGroup.addChild(horizontalAxis);
+                horizontalAxisGroup.addChild(labelGroup);
             }
-            
-            
-            this.drawLabels(axisPolygons, this.labels);
 
+            //draw vertical axes
+            var verticalAxisGroup = new paper.Group();
 
-            return axisPolygons;
+            for (var i = 1; i <= numOfSides; i ++) {
+                var startPoint = vertices[0],
+                    endPoint = vertices[vertices.length - 1 - i],
+                    k = (startPoint.y - endPoint.y) / (startPoint.x - endPoint.x),
+                    verticalAxis = new paper.Path.Line({
+                        from: startPoint,
+                        to: endPoint,
+                        strokeColor: strokeColor,
+                    });
+                verticalAxis.strokeColor.alpha = 0.4;
+
+                verticalAxisGroup.addChild(verticalAxis);
+            }
+
+            axesGroup.addChild(horizontalAxisGroup);
+            axesGroup.addChild(verticalAxisGroup);
+
+            return axesGroup;
         }
-        drawLabels(vertices, labels) {
-
+        drawLabel(data, position, horizontalAxis) {
+            return new paper.PointText({
+                point: horizontalAxis.getPointAt(position),
+                content: data,
+            });
         }
     }
     
     console.log(data.action);
     var actionGraph = new RadarGraph(data.action);
     var roleGraph = new RadarGraph(data.role);
+    console.log(actionGraph, roleGraph);
 
     //Place graphs on the right position
     var activeGraph = paper.project.activeLayer.children;
@@ -49,7 +100,7 @@ window.onload = function() {
             marginTop = paper.view.size.height / 2;
         activeGraph[i].position = new paper.Point(marginLeft, marginTop);
 
-        console.log(paper.view.size.width, activeGraph[i].position.x);
+        console.log(activeGraph);
     }
     
 }
